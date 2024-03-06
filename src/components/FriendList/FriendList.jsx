d9aed7eb5e9f04860d9af9b484ecc0703381ac61
zadumka <alenahas1987@gmail.com>
2024-03-06 12:18:22 +0200

    new hw

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import FriendListItem from '../FriendListItem/FriendListItem';
-import css from './FriendList.module.css';
+import css from './Friend.module.css';
 
 const FriendList = ({ friends }) => {
   return (
