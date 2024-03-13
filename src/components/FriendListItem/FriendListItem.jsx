6c695de191179d0c8adccfa579a9d8adb1f23818
zadumka <90329284+zadumka@users.noreply.github.com>
2024-03-13 13:02:03 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,30 @@
 // import css from './FriendListItem.module.css';
 
+// const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <div className={css.item}>
+//       <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
+//       <p className={css.name}>{name}</p>
+//       <p className={css[`${isOnline ? 'online' : 'offline'}`]}>
+//         {isOnline ? 'Online' : 'Offline'}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default FriendListItem;
+import React from 'react';
+
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusText = isOnline ? 'Online' : 'Offline';
+  const statusColor = isOnline ? 'green' : 'red';
+
   return (
-    <div className={css.item}>
-      <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
-      <p className={css.name}>{name}</p>
-      <p className={css[`${isOnline ? 'online' : 'offline'}`]}>
-        {isOnline ? 'Online' : 'Offline'}
-      </p>
+    <div className="friend-list-item">
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p style={{ color: statusColor }}>{statusText}</p>
     </div>
   );
 };
